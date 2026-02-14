37c128665640b2e85f874b7c9a8cc7b96dd71081
elvisanthonyy <info.elvisanthony@gmail.com>
2026-02-15 00:43:12 +0100

    text size

--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -4,9 +4,9 @@ const Nav = () => {
   return (
     <nav className="flex text-xl justify-between items-center z-10 px-[5%] absolute top-0 left-0 w-full h-18">
       <div className="">
-        <div className="font-bold">BeatsByYehuda</div>
+        <div className="font-bold text-2xl">BeatsByYehuda</div>
       </div>
-      <div className="w-10 h-10 rounded-full bg-gray-700"></div>
+      <div className="w-11 h-11 rounded-full bg-gray-700"></div>
     </nav>
   );
 };
